2d3281643cd3edeef911c57245a5272323bacb54
Maxim Nikolenko <maxim.nikolenko@gmail.com>
2010-02-21 19:10:31 +0200

    some styling

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -2,7 +2,7 @@ module CommentsHelper
 
   def comment_author_link(comment) 
     if comment.url.blank?
-      h(comment.author)
+      "<i>#{h(comment.author)}</i>"
     else
       link_to(h(comment.author), h(comment.url), :class => 'commentator-link', :rel => 'nofollow', :target => '_blank')
     end
